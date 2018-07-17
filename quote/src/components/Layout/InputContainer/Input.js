794bc2aae9b30d701f0db948222b504ecb49f162
bhuangbp <bhuang@baseplan.com>
2018-07-18 09:39:55 +1000

    feat(framework): setup project structure
    create components folder
    create reducers folder
    create css folder
    create InputContainer and QuoteContainer

---
+++ quote/src/components/Layout/InputContainer/Input.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class InputContainer extends Component {
+  render() {
+    return <div>This is input container</div>;
+  }
+}
+
+export default InputContainer;
---
+++ quote/src/components/Layout/QuoteContainer/Quote.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class Quote extends Component {
+  render() {
+    return <div>This is quote container</div>;
+  }
+}
+
+export default Quote;
