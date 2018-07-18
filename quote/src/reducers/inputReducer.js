fc2c4accfaa4e15bbeaf881ef2306b0c6a8dd60d
bhuangbp <bhuang@baseplan.com>
2018-07-18 16:09:25 +1000

    feat(web service): make web service call

--- quote/src/reducers/inputReducer.js
+++ quote/src/reducers/inputReducer.js
@@ -1,6 +1,9 @@
 import axios from "axios";
+import { WEBAPIURL } from "../utils/constants";
+
 const GET_QUOTE = "inputReducer/GET_QUOTE";
 const START_NEW_QUOTE = "inputReducer/START_NEW_QUOTE";
+
 const initialState = {
   firsrName: "",
   lastName: "",
@@ -12,13 +15,16 @@ const initialState = {
   amount: 0
 };
 
+const getQuote = (fromCurrency, toCurrency, amount) => {
+  let apiUrl = `${WEBAPIURL}/AUD/USD/10000?format=json`;
+};
+
 export default (state = initialState, action) => {
   switch (action.type) {
-
     case GET_QUOTE:
       return {
         ...state,
-        newQuote:action.newQuote
+        newQuote: action.newQuote
       };
 
     default:
