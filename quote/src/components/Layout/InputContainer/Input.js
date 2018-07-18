9cdbc133fc1cb0b6c7a3b897dca86567f83cd801
bhuangbp <bhuang@baseplan.com>
2018-07-18 15:59:45 +1000

    feat(input): add state to capture input data
    install axios for web api call
    set state for inpute components

--- quote/src/components/Layout/InputContainer/Input.js
+++ quote/src/components/Layout/InputContainer/Input.js
@@ -1,17 +1,54 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
+import InputReducer from "../../../reducers/inputReducer";
+import { bindActionCreators } from "redux";
 
 class Input extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
+    this.state = {
+      firsrName: "",
+      lastName: "",
+      email: "",
+      countryCode: "",
+      phoneNumber: "",
+      fromCurrency: "",
+      toCurrency: "",
+      amount: 0
+    };
+
+    this.handleGetQuoteClick = this.handleGetQuoteClick.bind(this);
+    this.handleInputChange = this.handleInputChange.bind(this);
   }
 
+  handleSubmit() {
+    console.log("submit fired!");
+  }
+
+  handleInputChange(event) {
+    const target = event.target;
+    const value = target.value;
+    const name = target.name;
+
+    this.setState({
+      [name]: value
+    });
+
+    console.log("name=", this.state.name);
+  }
+
+  handleGetQuoteClick(e) {
+    e.preventDefault();
+
+    console.log("state=", this.state);
+  }
 
   render() {
     return (
       <div className="rateForm col-12 col-md-6">
         <h1>Quick Quote</h1>
 
-        <form action="" className="rateForm_form">
+        <form onSubmit={this.handleSubmot} className="rateForm_form">
           <div className="rateForm_form_top">
             <div className="form-row">
               <div className="form-group col-md-6">
@@ -19,9 +56,11 @@ class Input extends Component {
                   First Name <span>*</span>
                 </label>
                 <input
+                  name="firstName"
                   type="text"
                   className="form-control"
-                  id=""
+                  value={this.state.firstName}
+                  onChange={this.handleInputChange}
                   placeholder="First Name"
                 />
               </div>
@@ -30,9 +69,11 @@ class Input extends Component {
                   Last Name <span>*</span>
                 </label>
                 <input
+                  name="lastName"
                   type="text"
                   className="form-control"
-                  id=""
+                  value={this.state.lastName}
+                  onChange={this.handleInputChange}
                   placeholder="Last Name"
                 />
               </div>
@@ -40,28 +81,36 @@ class Input extends Component {
             <div className="form-group">
               <label for="">Email</label>
               <input
+                name="email"
                 type="email"
                 class="form-control"
-                id=""
+                value={this.state.email}
+                onChange={this.handleInputChange}
                 placeholder="Email"
               />
             </div>
             <div class="form-row phone-number">
               <div class="form-group col-4 col-sm-3 col-md-3 col-lg-2">
-                <select className="form-control" id="">
+                <select
+                  className="form-control"
+                  name="countryCode"
+                  onChange={this.handleInputChange}
+                >
                   <option value="+61" selected>
                     +61
                   </option>
                   <option value="+61">+61</option>
-                  <option value="+61">+61</option>
-                  <option value="+61">+61</option>
+                  <option value="+91">+91</option>
+                  <option value="+86">+86</option>
                 </select>
               </div>
               <div className="form-group col-8 col-sm-9 col-md-9 col-lg-10">
                 <input
+                  name="phoneNumber"
                   type="text"
                   className="form-control"
-                  id=""
+                  value={this.state.phoneNumber}
+                  onChange={this.handleInputChange}
                   placeholder="Enter your phone"
                 />
               </div>
@@ -74,22 +123,36 @@ class Input extends Component {
                 <label for="">
                   From Currency <span>*</span>
                 </label>
-                <select className="form-control" id="">
-                  <option value="aud" selected>
+                <select
+                  className="form-control"
+                  name="fromCurrency"
+                  onChange={this.handleInputChange}
+                >
+                  <option value="USD" selected>
+                    United States Dollar (USD)
+                  </option>
+                  <option value="EUR">Euro (EUR)</option>
+                  <option value="GBP">British Pound (GBP)</option>
+                  <option value="AUD" selected>
                     Australian Dollar (AUD)
                   </option>
-                  <option value="aud">Australian Dollar (AUD)</option>
                 </select>
               </div>
               <div className="form-group col-md-6">
                 <label for="">
                   To Currency <span>*</span>
                 </label>
-                <select className="form-control" id="">
-                  <option value="usd" selected>
+                <select
+                  className="form-control"
+                  name="toCurrency"
+                  onChange={this.handleInputChange}
+                >
+                  <option value="USD" selected>
                     United States Dollar (USD)
                   </option>
-                  <option value="usd">United States Dollar (USD)</option>
+                  <option value="EUR">Euro (EUR)</option>
+                  <option value="GBP">British Pound (GBP)</option>
+                  <option value="AUD">Australian Dollar (AUD)</option>
                 </select>
               </div>
             </div>
@@ -99,16 +162,21 @@ class Input extends Component {
                   Amount <span>*</span>
                 </label>
                 <input
+                  name="amount"
                   type="text"
                   className="form-control"
-                  id=""
-                  value="25000"
-                  placeholder="25,000.00"
+                  value={this.state.amount}
+                  onChange={this.handleInputChange}
+                  placeholder="Amount"
                 />
               </div>
             </div>
             <div className="form-group text-center">
-              <button type="submit" className="btn">
+              <button
+                type="submit"
+                className="btn"
+                onClick={this.handleGetQuoteClick}
+              >
                 Get Quote
               </button>
             </div>
@@ -118,5 +186,9 @@ class Input extends Component {
     );
   }
 }
+
+const mapStateToProps = state => ({
+  firstName: state.inputReducer.firstName
+});
 
 export default Input;
--- quote/src/reducers/inputReducer.js
+++ quote/src/reducers/inputReducer.js
@@ -1,9 +1,26 @@
+import axios from "axios";
+const GET_QUOTE = "inputReducer/GET_QUOTE";
+const START_NEW_QUOTE = "inputReducer/START_NEW_QUOTE";
 const initialState = {
-  name: ""
+  firsrName: "",
+  lastName: "",
+  email: "",
+  countryCode: "",
+  phoneNumber: "",
+  fromCurrency: "",
+  toCurrency: "",
+  amount: 0
 };
 
 export default (state = initialState, action) => {
   switch (action.type) {
+
+    case GET_QUOTE:
+      return {
+        ...state,
+        newQuote:action.newQuote
+      };
+
     default:
       return state;
   }
